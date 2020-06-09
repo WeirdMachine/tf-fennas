ca0c598d635e24c0e46801a7608f876a2938b2ac
Matthias Osthues <osthues.matthias@gmail.com>
2020-06-09 11:48:09 +0200

    deploy openfaas via helm for arm
    Signed-off-by: Matthias Osthues <osthues.matthias@gmail.com>

---
+++ modules/openfaas/main.tf
@@ -0,0 +1,35 @@
+resource "kubernetes_namespace" "openfaas" {
+  metadata {
+    name = "openfaas"
+  }
+}
+
+resource "kubernetes_namespace" "openfaas_fn" {
+  metadata {
+    name = "openfaas-fn"
+  }
+}
+
+
+//TODO: remove nginx ingress annotion from openfaas-ingress
+resource "helm_release" "openfaas" {
+  name       = "openfaas"
+  repository = "https://openfaas.github.io/faas-netes"
+  chart      = "openfaas  "
+  namespace  = "openfaas"
+
+  set {
+    name  = "generateBasicAuth"
+    value = "true"
+  }
+
+  set {
+    name  = ""
+    value = ""
+  }
+
+  values = [
+    file("${path.module}/files/values-armhf.yml")
+  ]
+
+}
